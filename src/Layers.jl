52c19e856216e2df3cb711424024e2cbce7f34d4
catmanium <catmanium49@gmail.com>
2020-11-30 01:00:48 +0900

    Model.paramsで重み更新できるようにしたよ．

--- src/Layers.jl
+++ src/Layers.jl
@@ -4,77 +4,153 @@ module Layers
 命名規則
 ・活性化関数：Sigmoid
 ・損失関数：Sigmoid_with_CrossEntopy
+
+共通メンバ
+・params : 重み，バイアス
+・grads : 勾配
+
+共通メソッド
+・forward()
+・backward()
+
+損失レイヤ
+    ・メンバ
+        ・t : 教師データ
+        ・s : スコア
+    ・メソッド
+        ・forward() : t=[]ならスコア，あるなら損失を返す
 ==================#
 
 #params
 abstract type Params end
 mutable struct Sigmoid_params <: Params
+    params
+    grads
     out::Array
 end
 mutable struct Affine_params <: Params
-    W::Array{Float64,2}
-    b::Array{Float64,1}
+    params #W,b
+    grads #dW,db
     in
-    dW::Array{Float64,2}
-    v #momentam
-    s #RMSProp
 end
 mutable struct ReLU_params <: Params
+    params
+    grads
     mask_id
 end
 mutable struct Tanh_params <: Params
+    params
+    grads
     out::Array
 end
 mutable struct Sig_and_loss_params <: Params
+    params
+    grads
     s::Array #スコア
     t::Array
 end
 mutable struct mse_params <: Params
+    params
+    grads
     s::Array
     t::Array
 end
-mutable struct mae_params <: Params
-    s::Array
-    t::Array
+mutable struct RNN_params <: Params
+    Wx::Array{Float64,2}
+    Wh::Array{Float64,2}
+    b::Array{Float64,1}
+    dWx::Array{Float64,2}
+    dWh::Array{Float64,2}
+    cache
+end
+mutable struct TimeRNN_params <: Params
+    Wx::Array{Float64,2}
+    Wh::Array{Float64,2}
+    b::Array{Float64,1}
+    dWx::Array{Float64,2}
+    dWh::Array{Float64,2}
+    #複数のRNNレイヤを管理
+    layers
+    h
+    dh
+    stateful
+end
+mutable struct TimeAffine_params <: Params
+    W::Array{Float64,2}
+    b::Array{Float64,1}
+    dW::Array{Float64,2}
+    layers
+end
+mutable struct TimeMse_params <: Params
+    layers
 end
 
 
 #レイヤコンストラクタ
 function Sigmoid()
     out = []
-    return Sigmoid_params(out)
+    params = []
+    grads = []
+    return Sigmoid_params(params,grads,out)
 end
-function Affine(W::Array{Float64,2},b::Array{Float64,1})
-    in=[]
-    v = zeros(size(W))
-    s = zeros(size(W))
-    dW = zeros(size(W))
-    return Affine_params(W,b,in,dW,v,s)            
+function Affine(params,grads)
+    in=[] 
+    return Affine_params(params,grads,in)            
 end
 function ReLU()
+    params = []
+    grads = []
     mask_id = []
-    return ReLU_params(mask_id)
+    return ReLU_params(params,grads,mask_id)
 end
 function Tanh()
+    params = []
+    grads = []
     out = []
-    return Tanh_params(out)
+    return Tanh_params(params,grads,out)
 end
 function Sigmoid_with_CrossEntropy()
+    params = []
+    grads = []
     s = []
     t = []
-    return Sig_and_loss_params(s,t)
+    return Sig_and_loss_params(params,grads,s,t)
 end
 function Mean_Squared_Error()
+    params = []
+    grads = []
     s = []
     t = []
-    return mse_params(s,t)
+    return mse_params(params,grads,s,t)
+end
+function RNN(Wx::Array{Float64,2},Wh::Array{Float64,2},b::Array{Float64,1})
+    dWx = zeros(size(Wx))
+    dWh = zeros(size(Wh))
+    cache = []
+    return RNN_params(Wx,Wh,b,dWx,dWh,cache)
+end
+function TimeRNN(Wx::Array{Float64,2},Wh::Array{Float64,2},b::Array{Float64,1},stateful::Bool)
+    dWx = zeros(size(Wx))
+    dWh = zeros(size(Wh))
+    layers = nothing
+    dh = nothing
+    h = nothing
+    return TimeRNN_params(Wx,Wh,b,dWx,dWh,layers,dh,h,stateful)
+end
+function TimeAffine(W::Array{Float64,2},b::Array{Float64,1})
+    dW = zeros(size(W))
+    layers = []
+    return TimeAffine_params(W,b,dW,layers)
+end
+function TimeMse()
+    return TimeMse_params([])
 end
 
 
 #forward
 function forward(layer::Affine_params,in)
     layer.in = in #逆伝播に使う
-    return in * layer.W .+ layer.b'
+    return in * layer.params[1] .+ layer.params[2]'
 end
 function forward(layer::Sigmoid_params,in)
     out = 1.0 ./ (1.0 .+ exp.(-in)) #逆伝播に使う
@@ -115,11 +191,72 @@ function forward(layer::mse_params,in)
 
     return l
 end
+function forward(layer::RNN_params,x,h_prev)
+    t = h_prev*layer.Wh + x*layer.Wx .+ layer.b'
+    h_next = tanh.(t)
+
+    layer.cache = [x,h_prev,h_next]
+
+    return h_next
+end
+function forward(layer::TimeRNN_params,xs)
+    Wx = layer.Wx
+    Wh = layer.Wh
+    b = layer.b
+    #バッチサイズ，ブロック数，入力データ数
+    N, T, D = size(xs)
+    D, H = size(Wx)
+
+    layer.layers = []
+    hs = zeros(Float64,(N,T,H))
+
+    if !layer.stateful || layer.h === nothing
+        layer.h = zeros(Float64,(N,H))
+    end
+
+    for t in 1:T
+        rnn_layer = RNN(layer.Wx,layer.Wh,layer.b)
+        layer.h = forward(rnn_layer,xs[:,t,:],layer.h)
+        hs[:,t,:] = layer.h
+        push!(layer.layers,rnn_layer)
+    end
+
+    return hs
+end
+function forward(layer::TimeAffine_params,hs)
+    N,T,H = size(hs)
+    H,O = size(layer.W)
+    ys = zeros(Float64,(N,T,O))
+
+    for t in 1:T
+        h = hs[:,t,:]
+        affine_layer = Affine(layer.W,layer.b)
+        y = forward(affine_layer,h)
+        ys[:,t,:] = y
+        push!(layer.layers,affine_layer)
+    end
+
+    return ys
+end
+function forward(layer::TimeMse_params,ys,t_data)
+    N,T,O = size(ys)   
+    loss = zeros(Float64,size(ys))
+    
+    for t in 1:T
+        mse_layer = Mean_Squared_Error()
+        mse_layer.t = t_data[:,t,:]
+        y = ys[:,t,:]
+        loss[:,t,:] = forward(mse_layer,y)
+        push!(layer.layers,mse_layer)
+    end
+
+    return loss
+end
 
 #backward
 function backward(layer::Affine_params,din)
-    layer.dW = (layer.in)' * din
-    return din * (layer.W)'
+    layer.grads[1] .= (layer.in)' * din
+    return din * (layer.params[1])'
 end
 function backward(layer::ReLU_params,din)
     din[layer.mask_id[1:end]] .= 0
@@ -138,6 +275,89 @@ end
 function backward(layer::mse_params,din)
     return (layer.s - layer.t)
 end
+function backward(layer::RNN_params,dh_next)
+    Wx, Wh, b = layer.Wx, layer.Wh, layer.b
+    x, h_prev, h_next = layer.cache
+
+    dt = dh_next .* (1 .- h_next .^2)
+    dWh = h_prev' * dt
+    dh_prev = dt * Wh'
+    dWx = x' * dt
+    dx = dt * Wx'
+
+    layer.dWx = dWx
+    layer.dWh = dWh
+
+    return dx,dh_prev
+end
+function backward(layer::TimeRNN_params,dhs)
+    Wx = layer.Wx
+    Wh = layer.Wh
+    b = layer.b
+    N, T, H = size(dhs)
+    D, H = size(Wx)
+
+    dxs = zeros(Float64,(N,T,D))
+    dh = zeros(Float64,size(dhs[:,1,:]))
+
+    for t in T:-1:1
+        rnn_layer = layer.layers[t]
+        dx, dh = backward(rnn_layer,dhs[:,t,:]+dh)
+        dxs[:,t,:] = dx
+
+        #勾配合算
+        layer.dWx .+= rnn_layer.dWx
+        layer.dWh .+= rnn_layer.dWh
+    end
+
+    return dxs
     
+end
+function backward(layer::TimeAffine_params,dys)
+    N, T, O = size(dys)
+    H, O = size(layer.layers[1].W)
+    dhs = zeros(Float64,(N,T,H))
+
+    for t in 1:T
+        dh = zeros(Float64,(N,H))
+        dh = backward(layer.layers[t],dys[:,t,:])
+        dhs[:,t,:] = dh
+    end
+
+    return dhs
+end
+function backward(layer::TimeMse_params,dloss)
+    N, T, O = size(dloss)
+    dys = zeros(Float64,(N,T,O))
+
+    for t in 1:T
+        dy = zeros(Float64,(N,O))
+        dy = backward(layer.layers[t],0)
+        dys[:,t,:] = dy
+    end
+
+    return dys
+end
+
+
+#TimeRNN functions
+function set_state(layer::TimeRNN_params,h)
+    layer.h = h    
+end
+function reset_state(layer::TimeRNN_params)
+    layer.h = nothing
+end
+    
+#==============
+sample
+===============#
+mutable struct layer_a
+    params
+end
+
+function layer_f(params)
+    return layer_a(params)
+end
+
 
 end
--- src/Models.jl
+++ src/Models.jl
@@ -4,18 +4,54 @@ include("Layers.jl")
 include("Optimizer.jl")
 include("DataSet.jl")
 
-export MLP,predict,learn,optimizer,dataset
+export MLP,SimpleRNN,predict,learn,optimizer,dataset,model_f
 
 using Random
+
+#==============================
+共通メンバ変数
+・params : 各レイヤに重みをリスト化
+・grads : 各レイヤに勾配をリスト化 → Layers.forward()でLayerのメンバに代入したら合わせて変更される
+・layers : 各レイヤをリスト化
+・loss_layer : 損失関数レイヤ
+・optimizer : 最適化アルゴリズム
+
+共通メソッド
+・モデル名(;neuron,loss_layer) :コンストラクタ
+    ・neuron : 入力～中間～出力　までのニューロン数を指定
+    ・loss_layer : 損失関数（フルネーム指定）
+        ・重み，バイアス，レイヤ，損失関数レイヤの初期化
+        ・生成したレイヤからparams,gradsを取り出しメンバ変数へ保管
+
+・predict(model_params;data) : 推論　→　スコア算出
+
+・learn(model_params;batch_size,max_epoch,data,t_data=[]) 
+    ※以下をmax_epoch回ループ,max_ite回ループ
+    ・predict()実行　→　損失算出(loss_layerにt_dataが指定してあれば損失を返す)
+    ・Model.layersを逆からbackward() → 勾配算出
+    ・params更新
+
+
+===============================#
+
 
 #========
 Model_params
 ========#
 abstract type Model_params end
 mutable struct MLP_params <:Model_params
+    params
+    grads
     layers::Array
     optimizer
     debug::Bool
+end
+mutable struct SimpleRNN_params <:Model_params
+    params
+    grads
+    layers::Array
+    rnn_layer
+    optimizer
 end
 
 #==========
@@ -29,12 +65,25 @@ function MLP(;activation="Sigmoid",loss="Sigmoid_with_CrossEntropy",neuron,debug
     end
 
     optimizer = []
+    params = []
+    grads = []
+
     #レイヤ初期化
     layers = []
     for i in 1:length(neuron)-1
+        layers_tmp = []
+        params_tmp = []
+        grads_tmp = []
         #パラメータ生成
         W = randn(neuron[i],neuron[i+1])
         b = randn(neuron[i+1])
+        dW = zeros(Float64,size(W))
+        db = zeros(Float64,size(b))
+        #各レイヤのparams,gradsをリストで管理
+        params_tmp = [W,b]
+        grads_tmp = [dW,db]
+        append!(params,params_tmp)
+        append!(grads,grads_tmp)
 
         if i == length(neuron)-1
             #活性化関数&損失関数レイヤ
@@ -47,15 +96,38 @@ function MLP(;activation="Sigmoid",loss="Sigmoid_with_CrossEntropy",neuron,debug
 
         #レイヤ生成
         layers_tmp = [
-            Layers.Affine(W,b),
+            Layers.Affine(params_tmp,grads_tmp),
             eval(activation_layer),
        ]
 
        append!(layers,layers_tmp)
-
     end
 
-    return MLP_params(layers,optimizer,debug)
+    return MLP_params(params,grads,layers,optimizer,debug)
+end
+function SimpleRNN(;data_size,hidden_size,output_size)
+    #hidden_size RNNレイヤの隠れ状態の次元数
+    D, H, O = data_size, hidden_size, output_size
+
+    #重みの初期化
+    rnn_Wx = randn(D,H)/sqrt(D)
+    rnn_Wh = randn(H,H)/sqrt(H)
+    rnn_b = zeros(H)
+    affine_W = randn(H,O)/sqrt(H)
+    affine_b = zeros(O)
+    params = []
+    grads = []
+
+    #レイヤの初期化
+    layers = [
+        Layers.TimeRNN(rnn_Wx,rnn_Wh,rnn_b,true),
+        Layers.TimeAffine(affine_W,affine_b),
+        Layers.TimeMse()
+    ]
+    
+    optimizer = []
+
+    return SimpleRNN_params(params,grads,layers,layers[1],[])
 end
 
 
@@ -71,19 +143,24 @@ function predict(params::MLP_params,data)
     #推論と学習に応じて，スコアか損失か変わる(学習データの有無で判別)
     return out
 end
+function predict(params::SimpleRNN_params,data)
+    hs = Layers.forward(params.layers[1],data)
+    ys = Layers.forward(params.layers[2],hs)
+    return ys
+end
 
 
 #================
 学習
 ================#
-function learn(params::MLP_params;batch_size,epoch,data,t_data=[])
+function learn(this::MLP_params;batch_size,max_epoch,data,t_data=[])
     ite = size(data)[1] ÷ batch_size
     epoch_loss = 0
     min_loss = 0
     min_loss_ep = 0
     loss_list = [] #エポック毎の損失の平均を格納
     #1エポック=データを1巡
-    for i in 1:epoch
+    for i in 1:max_epoch
         ite_avg_loss_sum = 0 #1イテレーション毎の平均損失の総和
         #データのインデックスシャッフル
         idx = shuffle(1:size(data)[1])
@@ -93,24 +170,24 @@ function learn(params::MLP_params;batch_size,epoch,data,t_data=[])
 
         for k in 1:ite
             batch_data = data[1+(k-1)*batch_size:k*batch_size,1:end]
-            params.layers[end].t = t_data[1+(k-1)*batch_size:k*batch_size,1:end]
-            loss = predict(params,batch_data)
+            this.layers[end].t = t_data[1+(k-1)*batch_size:k*batch_size,1:end]
+            #順伝播
+            loss = predict(this,batch_data)
             #逆伝播
-            dout=0
-            for j in length(params.layers):-1:1
-                dout = Layers.backward(params.layers[j],dout)
+            dout=1
+            for j in length(this.layers):-1:1
+                dout = Layers.backward(this.layers[j],dout)
             end
             ite_avg_loss_sum += sum(loss)/length(loss) #1イテレーションの平均損失を足していく
 
             #更新
-            Optimizer.update(params.optimizer,params.layers)
-
+            Optimizer.update(this.optimizer,this)
         end
 
         epoch_avg_loss = ite_avg_loss_sum/ite #1エポック毎の平均損失
 
         #10回に一回出力する
-        if i%(epoch/10) == 0
+        if i%(max_epoch/10) == 0
             println("ep.$i : Loss :　",epoch_avg_loss)
         end
 
@@ -122,10 +199,54 @@ function learn(params::MLP_params;batch_size,epoch,data,t_data=[])
         append!(loss_list,epoch_avg_loss)
     end
 
-    params.layers[end].t = []
-
     println("min_loss : $min_loss")
     println("min_loss_ep : $min_loss_ep")
+
+    #predict単体で実行するために初期化しておく
+    this.layers[end].t = []
+
+    return loss_list
+end
+function learn(params::SimpleRNN_params;batch_size,max_epoch,window_size,data,t_data=[])
+    X = size(data,1) #総データ数
+    D = size(data,2) #データ次元数
+    T = window_size #RNNレイヤ数
+    N = batch_size #バッチ数
+
+    max_ite = (X/N)*(1/T) #イテレーション数
+    loss_list = [] #avg_lossのリスト
+
+    for epoch in 1:max_epoch
+        total_loss = 0 #損失合計
+        avg_loss = 0 #1エポックの平均損失
+        for ite in 1:max_ite
+            #ミニバッチ作成
+            xs = zeros(Float64,(N,T,D))
+            ts = zeros(Float64,(N,T,D))
+            for n in 1:N
+                st = 1+(T*(ite-1))+(X/N)*(n-1)
+                ed = T+(T*(ite-1))+(X/N)*(n-1)
+                xs[Int(n),:,:] = data[Int(st):Int(ed),:]
+                ts[Int(n),:,:] = t_data[Int(st):Int(ed),:]
+            end
+            #順伝播
+            ys = predict(params,xs)
+            loss = Layers.forward(params.layers[end],ys,ts)
+            if epoch==1 && ite==1
+                println(size(loss))
+            end
+            total_loss += sum(loss)
+            #逆伝播
+            dl = ones(Float64,size(loss))
+            dys = Layers.backward(params.layers[end],dl)
+            dhs = Layers.backward(params.layers[2],dys)
+            dxs = Layers.backward(params.layers[1],dhs)
+            #更新
+        end
+        avg_loss = total_loss/(T*D*N)
+        append!(loss_list,avg_loss)
+    end
+
 
     return loss_list
 end
@@ -133,11 +254,26 @@ end
 #================
 最適化 optimizer
 =================#
-function optimizer(params::Model_params;name="Adam",learning_rate=0.001,p1=0.95,p2=0.99,e=10^(-12))
-    hyper_params = [learning_rate,p1,p2,e]
+function optimizer(this::Model_params;name="Adam",learning_rate=0.001,p1=0.95,p2=0.99,e=10^(-12))
+    vs = []
+    ss = []
+    for i in 1:length(this.params)
+        v = zeros(Float64,size(this.params[i]))
+        s = zeros(Float64,size(this.params[i]))
+        append!(vs,[v])
+        append!(ss,[s])
+    end
+    hyper_params = [
+        learning_rate,
+        p1,
+        p2,
+        e,
+        vs,
+        ss
+    ]
     #optimizer生成
     optimizer_a = string("Optimizer.$name($hyper_params)")
-    params.optimizer = eval(Meta.parse(optimizer_a))
+    this.optimizer = eval(Meta.parse(optimizer_a))
 end
 
 #================
@@ -148,5 +284,35 @@ function dataset(type::String,n::Int64)
     type_function = Meta.parse(type_name)
     return eval(type_function)
 end
+
+
+#==============
+sample
+===============#
+mutable struct model_a
+    params
+    layers
+end
+
+function model_f()
+    params = []
+    layers = []
+    for i in 1:4
+        layers_tmp = []
+        params_tmp = []
+        W = randn(2,3)
+        b = randn(4)
+        #各レイヤのparams,gradsをリストで管理
+        params_tmp = [W,b]
+        append!(params,params_tmp)
+        layers_tmp = [
+            Layers.layer_f(params_tmp),
+        ]
+        append!(layers,layers_tmp)
+    end
+    
+    return model_a(params,layers)
+end
+
 
 end
